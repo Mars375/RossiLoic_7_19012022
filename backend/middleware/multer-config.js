c1fed54fa418cfd46b36454c69339fc28a51429e
Mars375 <loic_2610@hotmail.fr>
2024-08-27 15:25:26 +0200

    debug: cloudinary

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -3,6 +3,10 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 // Configurez Cloudinary avec vos informations d'API
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
