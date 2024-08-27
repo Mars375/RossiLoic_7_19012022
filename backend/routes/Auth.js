8ea04c22df4f47cdbc7411e0d7adc3d9f4ede37d
Mars375 <loic_2610@hotmail.fr>
2024-08-27 16:42:00 +0200

    fix:erase google route

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -6,7 +6,6 @@ const { requireAuth } = require('../middleware/auth')
 router.post('/signup', authCtrl.signup)
 router.post('/login', authCtrl.login)
 router.post('/logout', authCtrl.logout)
-router.post('/google', authCtrl.loginGoogle)
 router.get('/check-auth', requireAuth)
 
 module.exports = router
